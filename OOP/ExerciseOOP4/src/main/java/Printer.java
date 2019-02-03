a8a0f63f9e71c6d3f44b2976926bafb8714a98db
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2019-02-03 12:56:07 +0100

    training of polimorpishm

---
+++ OOP/ExerciseOOP4/src/main/java/Printer.java
@@ -0,0 +1,10 @@
+public class Printer implements Write {
+
+    public String writeText(String text) {
+        return text;
+    }
+
+    public String getMyName() {
+        return "Printer";
+    }
+}
---
+++ OOP/ExerciseOOP4/src/main/java/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+
+    public static void main(String[] args) {
+        Write writer = new Writer();
+        Write printer = new Printer();
+        Write typingMachine = new TypingMachine();
+    }
+}
