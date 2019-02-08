52c67d501cd8226c31e26d6325baaf535d1a6476
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2019-02-08 10:47:29 +0100

    implementation of recognize a seconds value

--- TDD/TDD1_BerlinClock/src/main/java/logic/impl/BerlinClock.java
+++ TDD/TDD1_BerlinClock/src/main/java/logic/impl/BerlinClock.java
@@ -6,15 +6,22 @@ public class BerlinClock implements Clock {
 
     /**
      * @param time in following format: HH:MM:SS shall be converted and returned as Berlin clock light bulb sequence
+     *             Seconds:
      *             00 -> O
-     *             01 -> R
+     *             01 -> Y
+     *             02 -> O
+     *             03 -> Y
+     *             ...
+     *             58 -> 0
+     *             59 -> Y
+     *
      * @return
      */
     public String convertTime(String time) {
-        //programowanie sterowane testami TDD
+        // TDD - Tested Driven Development - Programowanie Sterowane Testami
 
-        final int convertTime = Integer.parseInt(time);
+        final int seconds = time.charAt(4) + time.charAt(5);
 
-        return convertTime % 2 > 0 ? "R" : "O";
+        return seconds % 2 > 0 ? "Y" : "O";
     }
 }
--- TDD/TDD1_BerlinClock/src/test/java/logic/impl/BerlinClockTest.java
+++ TDD/TDD1_BerlinClock/src/test/java/logic/impl/BerlinClockTest.java
@@ -7,13 +7,13 @@ import static org.junit.Assert.*;
 public class BerlinClockTest {
 
     @Test
-    public void convertTime() {
+    public void convertSeconds() {
         //given
         final BerlinClock berlinClock = new BerlinClock();
         final String expectedLightMarkerEven = "O";
-        final String expectedLightMarkerOdd = "R";
-        final String even = "00";
-        final String odd = "01";
+        final String expectedLightMarkerOdd = "Y";
+        final String even = "00:00:00";
+        final String odd = "23:59:59";
         //when
         final String actualEven = berlinClock.convertTime(even);
         final String actualOdd = berlinClock.convertTime(odd);
