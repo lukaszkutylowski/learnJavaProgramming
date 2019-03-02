c34e4b86fe408f55dad85bc742e328c74553e736
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2019-03-02 18:18:51 +0100

    new class - Converter

--- TDD/TDD1_BerlinClock/src/main/java/logic/impl/Converter.java
+++ TDD/TDD1_BerlinClock/src/main/java/logic/impl/Converter.java
@@ -1,4 +1,19 @@
 package logic.impl;
 
 public class Converter {
+
+    public static int[] convertCharsToInt(String time) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int timeCharPosition = 0;
+        int arrayInt[] = new int[3];
+
+        for(int i = 0; i <= 2; i++) {
+            stringBuilder.append(time.charAt(timeCharPosition));
+            stringBuilder.append(time.charAt(timeCharPosition + 1));
+            arrayInt[i] = Integer.parseInt(stringBuilder.toString());
+            stringBuilder.setLength(0);
+            timeCharPosition += 3;
+        }
+        return arrayInt;
+    }
 }
