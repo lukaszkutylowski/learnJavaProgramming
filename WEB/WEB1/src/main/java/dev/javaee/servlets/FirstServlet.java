5790896488ab7631907cf694e1887d4284635f82
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2019-04-24 20:18:09 +0200

    first servlet which respond for do Get request

---
+++ WEB/WEB1/src/main/java/dev/javaee/servlets/FirstServlet.java
@@ -0,0 +1,25 @@
+package dev.javaee.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FirstServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setContentType("text/plain; charset=utf-8");
+        PrintWriter pw = res.getWriter();
+        pw.println("<html>");
+        pw.println("    <head>");
+        pw.println("        <title>First Servlet App</title>");
+        pw.println("    </head>");
+        pw.println("    <body>");
+        pw.println("        <p>first line: message 1</p>");
+        pw.println("        <p>second line: message 2</p>");
+        pw.println("    </body>");
+        pw.println("</html>");
+    }
+}
