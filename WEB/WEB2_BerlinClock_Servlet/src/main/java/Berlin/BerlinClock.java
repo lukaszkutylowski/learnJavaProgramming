e6061baf9bd8aefff49ebdf17aa756782a6636b2
Lukasz Kutylowski <kutylowski.lukasz.1@gmail.com>
2019-04-26 18:35:47 +0200

    implementation of input.jsp with html content

---
+++ WEB/WEB2_BerlinClock_Servlet/src/main/java/Berlin/BerlinClock.java
@@ -0,0 +1,50 @@
+package Berlin;
+
+import Resizable.Clock;
+import Resizable.LukCharList;
+
+public class BerlinClock implements Clock {
+
+    public BerlinClock() {
+    }
+
+    public String convertTime(String time) {
+        int[] timeInt = new int[3];
+        int timeCharIndexInString = 0;
+
+        for (int index = 0; index <= 2; index++) {
+            timeInt[index] = Converter.convertCharsToInt(time, timeCharIndexInString);
+            timeCharIndexInString += 3;
+        }
+
+        LukCharList list = new LukCharList();
+
+        processingSeconds(timeInt[2],list);
+        processingFiveHrs(timeInt[0],list);
+        processingSingleHrs(timeInt[0], list);
+        processingFiveMinutes(timeInt[1], list);
+        processingSingleMinutes(timeInt[1], list);
+
+        return list.toServletString();
+    }
+
+    private LukCharList processingSeconds(int secondsInt, LukCharList list) {
+        return EngineBerlinClock.convertSeconds(secondsInt, list);
+    }
+
+    private LukCharList processingFiveHrs(int hoursInt, LukCharList list) {
+        return EngineBerlinClock.convertFiveHrs(hoursInt, list);
+    }
+
+    private LukCharList processingSingleHrs(int hoursInt, LukCharList list) {
+        return EngineBerlinClock.convertIntToHoursOrMinutesString(hoursInt, Flag.HOURS, list);
+    }
+
+    private LukCharList processingFiveMinutes(int minutesInt, LukCharList list) {
+        return EngineBerlinClock.fiveMinutesLightBuilder(minutesInt, list);
+    }
+
+    private LukCharList processingSingleMinutes(int minutesInt, LukCharList list) {
+        return EngineBerlinClock.convertIntToHoursOrMinutesString(minutesInt, Flag.MINUTES, list);
+    }
+}
--- WEB/WEB2_BerlinClock_Servlet/src/main/java/ServletBerlinClock.java
+++ WEB/WEB2_BerlinClock_Servlet/src/main/java/ServletBerlinClock.java
@@ -1,10 +1,20 @@
+import Berlin.BerlinClock;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ServletBerlinClock extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String input = request.getParameter("input");
 
+        BerlinClock clock = new BerlinClock();
+        String output = clock.convertTime(input);
+
+        PrintWriter pw = response.getWriter();
+        pw.println(output);
     }
 }
